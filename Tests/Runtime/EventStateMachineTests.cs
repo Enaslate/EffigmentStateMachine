5949d8be7b0efbe216f4d4d3668edd1db9e9f7d5
enaslate <enaslate@gmail.com>
2026-05-16 20:45:36 +0300

    Add Event State Machine Tests

---
+++ Tests/Runtime/EventStateMachineTests.cs
@@ -0,0 +1,59 @@
+using Effigment.StateMachine.Core.Machines;
+using Effigment.StateMachine.Tests.Runtime.Stubs;
+using Effigment.StateMachine.Transitions;
+using NUnit.Framework;
+
+namespace Effigment.StateMachine.Tests.Runtime
+{
+    [TestFixture]
+    public class EventStateMachineTests
+    {
+        private EventStateMachine _machine;
+        private TestState _firstState;
+        private TestState _secondState;
+
+        private EventTransition _toFirstState;
+        private EventTransition _toSecondState;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _machine = new EventStateMachine();
+            _firstState = new TestState();
+            _secondState = new TestState();
+            _toFirstState = new EventTransition(null, _firstState, "toFirst");
+            _toSecondState = new EventTransition(_firstState, _secondState, "toSecond");
+            _machine.AddTransition(_toFirstState);
+            _machine.AddTransition(_toSecondState);
+        }
+
+        [Test]
+        public void Send_FromAnyState_Succusses()
+        {
+            _machine.Send(_toFirstState.Id);
+
+            Assert.That(_machine.Current, Is.EqualTo(_toFirstState.To));
+        }
+
+        [Test]
+        public void Send_FromCurrent_Successes()
+        {
+            _machine.Send(_toFirstState.Id);
+            _machine.Send(_toSecondState.Id);
+
+            Assert.That(_machine.Current, Is.EqualTo(_toSecondState.To));
+        }
+
+        [Test]
+        public void Send_WhenCurrentIsNotFrom_Successes()
+        {
+            var thirdState = new TestState();
+            var toThird = new EventTransition(_secondState, thirdState, "toThird");
+
+            _machine.Send(_toFirstState.Id);
+            _machine.Send(toThird.Id);
+
+            Assert.That(_machine.Current, Is.EqualTo(_firstState));
+        }
+    }
+}
